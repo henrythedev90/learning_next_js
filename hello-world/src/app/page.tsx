00e510c0c4aeb7a8768cea461204018b4520dd18
henrythedev90 <henrythedev90@gmail.com>
2024-09-19 01:00:32 -0400

    navigating from the home page to other pages

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
+
 export default function Home(){
-    return<h1>Homepage</h1>
+    return(<>
+    <h1>Homepage</h1>
+    <Link href={"/blog"}>Blog</Link>
+    <Link href={"/product"}>Product</Link>
+    </>)
 }
