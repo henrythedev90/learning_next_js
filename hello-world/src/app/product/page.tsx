e2b74a8a58827f521408023d478f34aef248f02f
henrythedev90 <henrythedev90@gmail.com>
2024-09-19 01:05:29 -0400

    string manipulation on link nav

--- hello-world/src/app/product/page.tsx
+++ hello-world/src/app/product/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
 
 export default function ProductList(){
+    const stringManipulation = 3
     return(
         <>
         <Link href={'/'}>Home</Link>
         <h1>Product List</h1>
         <h2><Link href={'/product/1'}>Product 1</Link></h2>
         <h2><Link href={'/product/2'}>Product 2</Link></h2>
-        <h2><Link href={'/product/3'}>Product 3</Link></h2>
+        <h2><Link href={`/product/${stringManipulation}`}>Product 3</Link></h2>
         </>
     )
 }
