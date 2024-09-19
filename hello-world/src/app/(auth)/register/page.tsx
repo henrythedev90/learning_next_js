4d2feec7e3f967983bb6fb120482a0db87a4e988
henrythedev90 <henrythedev90@gmail.com>
2024-09-19 16:54:06 -0400

    added prettier

--- hello-world/src/app/(auth)/register/page.tsx
+++ hello-world/src/app/(auth)/register/page.tsx
@@ -1,3 +1,3 @@
-export default function Register(){
-    return <h1>This is the Register page</h1>
+export default function Register() {
+  return <h1>This is the Register page</h1>;
 }
--- hello-world/src/app/Footer.tsx
+++ hello-world/src/app/Footer.tsx
@@ -1,5 +1,7 @@
-export default function Footer(){
-    return(
-        <footer style={{backgroundColor: 'pink', padding: '1rem'}}><p>This is the footer</p></footer>
-    )
+export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: "pink", padding: "1rem" }}>
+      <p>This is the footer</p>
+    </footer>
+  );
 }
