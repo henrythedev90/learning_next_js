8fed1f6f6bb7aeb98a76cf8e44c70f68b9fe8981
henrythedev90 <henrythedev90@gmail.com>
2024-09-19 01:03:37 -0400

    nested routing

--- hello-world/src/app/product/page.tsx
+++ hello-world/src/app/product/page.tsx
@@ -5,9 +5,9 @@ export default function ProductList(){
         <>
         <Link href={'/'}>Home</Link>
         <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2><Link href={'/product/1'}>Product 1</Link></h2>
+        <h2><Link href={'/product/2'}>Product 2</Link></h2>
+        <h2><Link href={'/product/3'}>Product 3</Link></h2>
         </>
     )
 }
