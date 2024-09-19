436bb4f1098deaee517f17dd003396c79fbb01fe
henrythedev90 <henrythedev90@gmail.com>
2024-09-19 01:22:46 -0400

    changed the name of the array

--- hello-world/src/app/product/page.tsx
+++ hello-world/src/app/product/page.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link"
 
 export default function ProductList(){
-    const secondHalf = [1,2,3,4,5,6]
-
-       
+    const lengthOfLinks = [1,2,3,4,5,6]
     return(
         <>
         <Link href={'/'}>Home</Link>
         <h1>Product List</h1>
-        {secondHalf.map((el) =>{
+        {lengthOfLinks.map((el) =>{
             return(
                 <h2 key={el}><Link href={`/product/${el}`}>Product {el}</Link></h2>
             )
